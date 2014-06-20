6c1cac4c5658d2480e12057b4db43ed3d5d6c771
Paul Walkington <pwalkington@williamhill.co.uk>
2014-06-20 10:23:33 +0100

    added validation to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
 	belongs_to :article
+	validates :commenter, presence: true, length: { minimum: 1 }
+	validates :body, presence: true, length: { minimum: 1 }
 end
+
